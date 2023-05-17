6a466ab19da2258d884740f3593000840c9af269
Alan <chenyujen0218@gmail.com>
2023-05-17 16:26:48 +0800

    update from PC 2nd time 1120517

--- useref-my-app/src/App.js
+++ useref-my-app/src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from 'react'
 
 function App() {
+  const [text, setText] = useState('')
+  const renderCount = useRef(1) //{ current : 0 }
+  // 宣告 useRef 變數: 連結 DOM & REACT 
+  const inputRef = useRef(null)
+  // 宣告 useRef 變數: 儲存前一個輸入的值
+  const prevInput = useRef('') //{ current : ''}
+
+  useEffect(() => {renderCount.current += 1});
+
+
+  // 將當前狀態儲存到 prevInput.current 陣列中，useEffect 的回調函數是在渲染完成之後執行的，因此會保留並可顯示在渲染後。
+  useEffect(()=>{ prevInput.current = text},[text])
+
+  // 點擊事件的函式
+  function focusBtn(){
+
+    console.log(inputRef.current); 
+    inputRef.current.focus();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input ref={inputRef} type="text" onChange={(e) => setText(e.target.value)}></input>
+      <div>You're typing {text}, and you used to type {prevInput.current}.</div>
+      <div>Render { renderCount.current } time.</div>
+      <button onClick={focusBtn}>自動聚焦</button>
+    </>
   );
 }
 
