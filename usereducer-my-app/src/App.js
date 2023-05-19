677407b850a29f0a06e734a7ecda3cdd6e01fe12
Alan <chenyujen0218@gmail.com>
2023-05-19 16:27:54 +0800

    update from PC 1120519

--- usereducer-my-app/src/App.js
+++ usereducer-my-app/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useReducer } from 'react'
 
 function App() {
+
+  const [count, setCount] = useState(0);
+  /*useReducer syntax: 
+  const [state,dispatch]=useReducer(reducer,initialState)。
+  
+  dispatch 是一個函數，用來發送 action，reducer 是一個函數，用來處理 state 的變化。*/
+
+  function add() {
+    setCount(prevCount => prevCount + 1);
+  }
+  function minus() {
+    setCount(prevCount => prevCount - 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <button onClick={minus}>-</button>
+        <span value={count}>{count}</span>
+        <button onClick={add}>+</button>
+      </div>
+    </>
   );
 }
 
