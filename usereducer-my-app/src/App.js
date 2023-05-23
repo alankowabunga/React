2958c4f60b96b50f4cd4211b73bb59fb726eebb3
Alan <babyalan0857@gmail.com>
2023-05-23 23:04:18 +0800

    scroll trigger 1120523

--- usereducer-my-app/src/App.js
+++ usereducer-my-app/src/App.js
@@ -2,24 +2,36 @@ import React, { useState, useReducer } from 'react'
 
 function App() {
 
-  const [count, setCount] = useState(0);
   /*useReducer syntax: 
   const [state,dispatch]=useReducer(reducer,initialState)。
   
   dispatch 是一個函數，用來發送 action，reducer 是一個函數，用來處理 state 的變化。*/
+  const [state,dispatch] = useReducer(reducer,{ count:0 })
+
+  function reducer(state,action){
+    switch (action.type){
+      case 'add':
+        return({count: state.count +1})
+        case 'minus':
+          return({count:state.count-1})
+      default:
+        return state
+    }
+  }
 
   function add() {
-    setCount(prevCount => prevCount + 1);
+    dispatch({type:"add"});  
+    //將 type 屬性發送到 action 調用 reducer函式，進行 switch 判斷
   }
   function minus() {
-    setCount(prevCount => prevCount - 1);
+    dispatch({type:"minus"});
   }
 
   return (
     <>
       <div>
         <button onClick={minus}>-</button>
-        <span value={count}>{count}</span>
+        <span >{state.count}</span>
         <button onClick={add}>+</button>
       </div>
     </>
