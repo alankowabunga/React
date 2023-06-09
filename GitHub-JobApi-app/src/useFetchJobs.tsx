3d370efd26792e930f49273d96db152da07831c5
Alan <chenyujen0218@gmail.com>
2023-06-09 16:27:05 +0800

    123

--- GitHub-JobApi-app/src/useFetchJobs.tsx
+++ GitHub-JobApi-app/src/useFetchJobs.tsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { useReducer } from "react";
 
+const ACTIONS = {
+    MAKE_REQUEST: "make-request",
+    GET_DATA: "get-data",
+    ERROR: "error",
+};
 
 export const useFetchJobs = () => {
-  return (
-    {
-      jobs:[],
-      loading:false,
-      error:false
-    }
-  )
-}
+    return {
+        jobs: [],
+        loading: false,
+        error: false,
+    };
+};
