c352b1e1ec7b30b06525efe997443514bcf78df0
Alan <chenyujen0218@gmail.com>
2023-06-13 16:29:38 +0800

    input

--- Form-Validation-app/src/Components/Register.tsx
+++ Form-Validation-app/src/Components/Register.tsx
@@ -2,30 +2,100 @@ import React, { useRef, useEffect, useState } from "react";
 import "../register.css";
 import "bootstrap/dist/css/bootstrap.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+import {
+    faInfoCircle,
+    faTimes,
+    faCheck,
+} from "@fortawesome/free-solid-svg-icons";
 
 const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{6,23}$/;
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
 
 export const Register = () => {
+    const userRef = useRef(null); //user input
+    const errorRef = useRef(null); // 如果有 error 要把滑鼠焦點放到錯誤格子(??)
+
+    const [user, setUser] = useState("");
+    const [validName, setValidName] = useState(false); //是否符合格式要求
+    const [userFocus, setUserFocus] = useState(false); // 沒有聚焦的時候不用顯示提示框(??)
+
+    const [pwd, setPsw] = useState("");
+    const [validPwd, setValidPwd] = useState(false);
+    const [pwdFocus, setPwdFocus] = useState(false);
+
+    const [matchPwd, setMatchPwd] = useState("");
+    const [validMatch, setValidMatch] = useState(false);
+    const [matchFocus, setMatchFocus] = useState(false);
+    /*
+    const [errMsg,setErrMsg]=useState('');
+    const [success,setSuccess]=useState(false);
+
+    useEffect(()=>{
+        setErrMsg('');
+    },[user,pwd,matchPwd])
+    */
+
+    useEffect(() => {
+        userRef.current.focus();
+        // 利用 useEffect 的 dependency 設 [] 空陣列時會在元件第一次渲染後執行一次的特性、將滑鼠聚焦在輸入框。
+    }, []);
+    // 檢查 Username 格式 -> validName
+    useEffect(() => {
+        const boolean = USER_REGEX.test(user);
+        setValidName(boolean);
+    }, [user]);
+    // 檢查 psw、pswChk 格式 validPwd、validMatch
+    useEffect(() => {
+        const formatBoolean = PWD_REGEX.test(pwd);
+        setValidPwd(formatBoolean);
+        const match = pwd == matchPwd;
+        setValidMatch(match);
+    }, [pwd, matchPwd]);
+
     return (
-        <section className="bg-primary p-5 rounded">
+        <section className="container p-5 rounded">
             <p className=""></p>
             <h1 className="text text-white-50">Register</h1>
             <form className="form">
                 <div className="form-group my-4">
                     <label htmlFor="username" className="text text-white fs-5">
-                        username:
+                        Username:
+                        <FontAwesomeIcon
+                            icon={faCheck}
+                            className={`${
+                                validName ? "show" : "hide"
+                            }  text-success icon`}
+                        />
+                        <FontAwesomeIcon
+                            icon={faTimes}
+                            className={`${
+                                validName || !user ? "hide" : "show"
+                            }  text-danger icon`}
+                        />
                     </label>
                     <input
+                        ref={userRef}
+                        value={user}
+                        onChange={(e) => setUser(e.target.value)}
                         className="form-control"
                         type="text"
                         id="username"
                         required
+                        onFocus={() => setUserFocus(true)}
+                        onBlur={() => setUserFocus(false)}
                     />
-                    <p className="hint">
-                        <FontAwesomeIcon icon={faInfoCircle} className="" />
-                        4 to 24 characters.
+                    <p
+                        className={`${
+                            user && userFocus && !validName
+                                ? "instruction"
+                                : "offScreen"
+                        } form-control hint text-center mt-1 bg-dark rounded text-white`}
+                    >
+                        <FontAwesomeIcon
+                            icon={faInfoCircle}
+                            style={{ marginRight: "10px" }}
+                        />
+                        6 to 24 characters.
                         <br />
                         Must begin with a letter.
                     </p>
@@ -33,26 +103,94 @@ export const Register = () => {
 
                 <div className="form-group my-4">
                     <label htmlFor="password" className="text text-white fs-5">
-                        password:
+                        Password:
+                        <FontAwesomeIcon
+                            icon={faCheck}
+                            className={`${
+                                validPwd ? "show" : "hide"
+                            }  text-success icon`}
+                        />
+                        <FontAwesomeIcon
+                            icon={faTimes}
+                            className={`${
+                                validPwd || !pwd ? "hide" : "show"
+                            }  text-danger icon`}
+                        />
                     </label>
                     <input
                         className="form-control"
                         type="text"
                         id="password"
+                        onChange={(e) => setPsw(e.target.value)}
+                        value={pwd}
+                        onFocus={() => setPwdFocus(true)}
+                        onBlur={() => setPwdFocus(false)}
                         required
                     />
+                    <p
+                        className={`${
+                            pwd && pwdFocus && !validPwd
+                                ? "instruction"
+                                : "offScreen"
+                        } hint text-center mt-1 bg-dark rounded text-white`}
+                    >
+                        <FontAwesomeIcon
+                            icon={faInfoCircle}
+                            style={{ marginRight: "10px" }}
+                        />
+                        Must contains
+                        <br /> 8 to 24 characters、
+                        <br />
+                        one UpperCase
+                        <br />
+                        one LowerCase
+                        <br />
+                        one number
+                        <br />
+                        one Special character.{" "}
+                    </p>
                 </div>
 
                 <div className="form-group my-4">
                     <label htmlFor="pswChk" className="text text-white fs-5">
                         Confirm Password:
+                        <FontAwesomeIcon
+                            icon={faCheck}
+                            className={`${
+                                validMatch && matchPwd ? "show" : "hide"
+                            }  text-success icon`}
+                        />
+                        <FontAwesomeIcon
+                            icon={faTimes}
+                            className={`${
+                                validMatch || !matchPwd ? "hide" : "show"
+                            }  text-danger icon`}
+                        />
                     </label>
                     <input
                         className="form-control"
                         type="text"
                         id="pswChk"
+                        onChange={(e) => setMatchPwd(e.target.value)}
+                        onFocus={() => setMatchFocus(true)}
+                        onBlur={() => setMatchFocus(false)}
                         required
                     />
+                    <p
+                        className={`${
+                            matchPwd && matchFocus && !validMatch
+                                ? "instruction"
+                                : "offScreen"
+                        } hint text-center mt-1 bg-dark rounded text-white `}
+                    >
+                        <FontAwesomeIcon
+                            icon={faInfoCircle}
+                            style={{ marginRight: "10px" }}
+                        />
+                        Password Unmatch.
+                        <br />
+                        Please verify again.
+                    </p>
                 </div>
 
                 <button className="btn btn-outline-warning btn-lg">
